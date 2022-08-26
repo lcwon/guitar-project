90a29ca1ee670d9378ba767fa5f34e295d26f4ca
young-sia <youngly1028@gmail.com>
2022-08-26 18:17:37 +0900

    finals ver.

---
+++ future_updates/feature_updates.py
@@ -0,0 +1,58 @@
+
+
+## --------------- For Chord module --------------- ##
+#: Table of major chord to corresponding enharmonic chord (for ``chord`` module)
+ENHARMONIC_TABLE = {'Cb': 'B', 'Db': 'C#', 'Eb': 'D#', 'Fb': 'E', 'Gb': 'F#', 'Ab': 'G#', 'Bb': 'A#'}
+
+#: Mapping of chord names to integers (for ``chord`` module).
+CHORD_UPDATED = {
+    'C:maj': 0,
+    # 'C#:maj': 1,
+    'D:maj': 2,
+    # 'D#:maj': 3,
+    'E:maj': 4,
+    'F:maj': 5,
+    # 'F#:maj': 6,
+    'G:maj': 7,
+    # 'G#:maj': 8,
+    'A:maj': 9,
+    # 'A#:maj': 10,
+    'B:maj': 11,
+    'C:min': 12,
+    # 'C#:min': 13,
+    'D:min': 14,
+    # 'D#:min': 15,
+    'E:min': 16,
+    'F:min': 17,
+    # 'F#:min': 18,
+    'G:min': 19,
+    # 'G#:min': 20,
+    'A:min': 21,
+    # 'A#:min': 22,
+    'B:min': 23,
+    'N': 24,
+    'X': 25,
+    'C7': 26,
+    'C7:maj': 27,
+    'C7:min': 28,
+    'D7': 29,
+    'D7:maj': 30,
+    'D7:min': 31,
+    'E7': 32,
+    'E7:maj': 33,
+    'E7:min': 34,
+    'F7': 35,
+    'F7:maj': 36,
+    'F7:min': 37,
+    'G7': 38,
+    'G7:maj': 39,
+    'G7:min': 40,
+    'A7': 41,
+    'A7:maj': 42,
+    'A7:min': 43,
+    'B7': 44,
+    'B7:maj': 45,
+    'B7:min': 46,
+
+    # THINK ABOUT FRACTION CODE TOO
+}
